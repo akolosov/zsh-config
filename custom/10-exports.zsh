070381f909a218c9007f829504594898919756ee
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-03-19 09:28:05 +0600

    Небольшие правки.

--- custom/10-exports.zsh
+++ custom/10-exports.zsh
@@ -3,13 +3,20 @@ if [ -f /var/cache/ruby-rvm/environments/default ]; then
     . /var/cache/ruby-rvm/environments/default
 fi
 
+if [ -f /usr/share/ruby-rvm/environments/default ]; then
+    . /usr/share/ruby-rvm/environments/default
+fi
+
+if [ -f /usr/local/rvm/environments/default ]; then
+    . /usr/local/rvm/environments/default
+fi
+
 if [ $HOST = "ASU1" ]; then
-    export http_proxy=http://hunter:bumpy@192.168.0.224:3128/
-    export https_proxy=http://hunter:bumpy@192.168.0.224:3128/
-    export ftp_proxy=http://hunter:bumpy@192.168.0.224:3128/
     export no_proxy=$(echo $no_proxy | sed 's/,$//')
 elif [ $HOST = "YODA" ]; then
     export no_proxy=$(echo $no_proxy | sed 's/,$//')
 fi
 
 export REALLY_GEM_UPDATE_SYSTEM='yes'
+
+export PATH; PATH="/opt/local/bin:$PATH"
