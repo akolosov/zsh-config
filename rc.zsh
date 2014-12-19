f343ba0ae3a6dc17d2cf197c68751d67ee1acb15
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-12-19 13:09:23 +0500

    FIX

--- rc.zsh
+++ rc.zsh
@@ -25,7 +25,7 @@ USE_ZKBD="false"
 
 # Which plugins would you like to load? (plugins can be found in ~/.zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git github git-extras git-flow rails3 debian ruby rvm rake bundler gem extract kate )
+plugins=(git github git-extras git-flow go golang autoenv brew brew-cask rails debian ruby rvm rake bundler gem extract)
 
 source $ZSH/configure.zsh
 
