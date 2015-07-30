287f4d5159657476e23d499c197ddd076c521cad
Alexey Kolosov <alexey.kolosov@gmail.com>
2015-07-30 23:45:54 +0500

    +VirtualEnv support

--- custom/10-exports.zsh
+++ custom/10-exports.zsh
@@ -1,3 +1,19 @@
+# Misc config
+export REALLY_GEM_UPDATE_SYSTEM='yes'
+
+export JAVA_HOME="/usr/lib/jvm/jre1.7.0_21"
+
+export PATH="/usr/local/bin:/opt/local/bin:$PATH:$JAVA_HOME/bin"
+
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd_hbgr -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
+
+export EDITOR="vim"
+
+eval "$(thefuck-alias)"
+
+#VirtualEnv config
+[ -f ~/Development/Python3env/bin/activate ] && . ~/Development/Python3env/bin/activate
+
 #RVM Config
 [ -f /var/cache/ruby-rvm/environments/default ] && . /var/cache/ruby-rvm/environments/default
 
@@ -16,12 +32,3 @@
 
 [ -f ~/.gvm_local ] && . ~/.gvm_local
 
-export REALLY_GEM_UPDATE_SYSTEM='yes'
-
-export JAVA_HOME="/usr/lib/jvm/jre1.7.0_21"
-
-export PATH="/usr/local/bin:/opt/local/bin:$PATH:$JAVA_HOME/bin"
-
-export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd_hbgr -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
-
-export EDITOR="vim"
