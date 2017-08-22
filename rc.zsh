585ab69ec514e791c4cba014986c18fc88314141
Alexey Kolosov <alexey.kolosov@gmail.com>
2017-08-22 15:37:57 +0500

    FIX

--- rc.zsh
+++ rc.zsh
@@ -38,7 +38,7 @@ PATH=$PATH:/usr/local/rvm/bin
 # enable xon terminal - vim Ctrl-S bug
 stty -ixon
 
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
